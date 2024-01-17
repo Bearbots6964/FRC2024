40d5012be461d7c1273ec20f94f86e6d0db07ccc
gaming <48131223+TheGamer1002@users.noreply.github.com>
2024-01-17 15:37:43 -0500

    fix: import for ClimbCommand

--- src/main/java/frc/robot/commands/ClimbCommand.java
+++ src/main/java/frc/robot/commands/ClimbCommand.java
@@ -1,6 +1,7 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.ClimberSubsystem;
 
 public class ClimbCommand extends Command {
   private final ClimberSubsystem climberSubsystem = ClimberSubsystem.getInstance();
