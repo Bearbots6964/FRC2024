2b9f26cf2ecee756d5ad0d71d7060cc36012546d
gaming <48131223+TheGamer1002@users.noreply.github.com>
2024-01-27 11:47:18 -0500

    feat(logging): create new telemetry subsystem, add NetworkAlerts interface

---
+++ src/main/java/frc/robot/subsystems/TelemetrySubsystem.java
@@ -0,0 +1,41 @@
+package frc.robot.subsystems;
+
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class TelemetrySubsystem extends SubsystemBase {
+
+  // With eager singleton initialization, any static variables/fields used in the
+  // constructor must appear before the "INSTANCE" variable so that they are initialized
+  // before the constructor is called when the "INSTANCE" variable initializes.
+
+  /**
+   * The Singleton instance of this TelemetrySubsystem. Code should use
+   * the {@link #getInstance()} method to get the single instance (rather
+   * than trying to construct an instance of this class.)
+   */
+  private final static TelemetrySubsystem INSTANCE = new TelemetrySubsystem();
+
+  /**
+   * Returns the Singleton instance of this TelemetrySubsystem. This static method
+   * should be used, rather than the constructor, to get the single instance
+   * of this class. For example: {@code TelemetrySubsystem.getInstance();}
+   */
+  @SuppressWarnings("WeakerAccess")
+  public static TelemetrySubsystem getInstance() {
+    return INSTANCE;
+  }
+
+  /**
+   * Creates a new instance of this TelemetrySubsystem. This constructor
+   * is private since this class is a Singleton. Code should use
+   * the {@link #getInstance()} method to get the singleton instance.
+   */
+  private TelemetrySubsystem() {
+    // TODO: Set the default command, if any, for this subsystem by calling setDefaultCommand(command)
+    //       in the constructor or in the robot coordination class, such as RobotContainer.
+    //       Also, you can call addChild(name, sendableChild) to associate sendables with the subsystem
+    //       such as SpeedControllers, Encoders, DigitalInputs, etc.
+  }
+}
+
