e3ca710ab0db024f407b9de99623b25b8477a4b6
Alexandra Reaves <nyxalexandra@proton.me>
2024-02-07 15:55:07 -0500

    refactor!(intake): rename methods and reorder fields

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -8,31 +8,12 @@ import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class IntakeSubsystem extends SubsystemBase {
+  /** A limit switch to determine whether the intake has a note or not. */
+  DigitalInput limitSwitch = new DigitalInput(0);
+  public CANSparkMax intakeMotor;
 
-  // With eager singleton initialization, any static variables/fields used in the
-  // constructor must appear before the "INSTANCE" variable so that they are initialized
-  // before the constructor is called when the "INSTANCE" variable initializes.
-
-  /**
-   * The Singleton instance of this IntakeSubsystem. Code should use the {@link #getInstance()}
-   * method to get the single instance (rather than trying to construct an instance of this class.)
-   */
   private static final IntakeSubsystem INSTANCE = new IntakeSubsystem();
 
-  // A limit switch to determine whether the intake has a note or not.
-  DigitalInput limitSwitch = new DigitalInput(0);
-
-  /**
-   * Returns the Singleton instance of this IntakeSubsystem. This static method should be used,
-   * rather than the constructor, to get the single instance of this class. For example: {@code
-   * IntakeSubsystem.getInstance();}
-   */
-  @SuppressWarnings("WeakerAccess")
-  public static IntakeSubsystem getInstance() {
-    return INSTANCE;
-  }
-
-  public CANSparkMax intakeMotor;
   private IntakeSubsystem() {
    intakeMotor = new CANSparkMax(8, MotorType.kBrushless);
     intakeMotor.setIdleMode(IdleMode.kBrake);
@@ -40,15 +21,20 @@ public class IntakeSubsystem extends SubsystemBase {
     intakeMotor.burnFlash();
   }
 
-  public void SuckOut() {
+  @SuppressWarnings("WeakerAccess")
+  public static IntakeSubsystem getInstance() {
+    return INSTANCE;
+  }
+
+  public void suckOut() {
     intakeMotor.set(0.75);
   }
 
-  public void SuckIn() {
+  public void suckIn() {
     intakeMotor.set(-0.75);
   }
 
-  public void Stop() {
+  public void stop() {
     intakeMotor.set(0);
   }
 }
