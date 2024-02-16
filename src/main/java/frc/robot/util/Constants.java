df17e2f7c3ab96d75354182758f4e08ce71aa2d9
gaming <48131223+TheGamer1002@users.noreply.github.com>
2024-02-15 23:23:06 -0500

    feat(swerve): results of first testing day

--- src/main/java/frc/robot/util/Constants.java
+++ src/main/java/frc/robot/util/Constants.java
@@ -54,6 +54,9 @@ public final class Constants {
     public static final double JOYSTICK_TWIST_DEADBAND = 0.1;
 
     public static final double TURN_CONSTANT = 6;
+    public static final double LEFT_Y_DEADBAND = 0.1;
+    public static final double LEFT_X_DEADBAND = 0.1;
+    public static final double RIGHT_X_DEADBAND = 0.1;
   }
 
   public static class MotorConstants {
